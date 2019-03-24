bfec92f7bd527158258f4da4cd7f81cb4ead39ca
AndikaPerdhana47 <andika.perdhana99@gmail.com>
2019-03-24 21:19:50 +0700

    Tambahan E-Dokumen dari Gugum

---
+++ page/EDoc.php
@@ -0,0 +1,323 @@
+<link rel="stylesheet" href="page/css2/cssEDoc.css">
+<link rel="stylesheet" href="page/css2/cssMagnify.css">
+
+<div id="button"></div>
+<div class ="container-fluid">
+    <div class="row">
+        <div class="col-sm-12 col-md-12 col-xl-12">
+        <div class="btn-group btn-breadcrumb">
+            <a href="?page=beranda" class="btn btn-info"><i class="glyphicon glyphicon-home"></i></a>
+            <span class="btn btn-info btn1"><strong>E-Dokumen</strong></span>
+         </div>
+         <hr class="hr-primary hr1" />
+        </div> 
+    </div>
+    <div class="container con1">
+        <h2>E-Dokumen</h2>
+	    <div class="panel panel-info">
+        <div class="panel-heading">
+            <h3 class="panel-title">List E-Dokumen yang Bisa Diunduh</h3>
+        </div>   
+        <ul class="list-group">
+            <li class="list-group-item">
+                <div class="row toggle" id="dropdown-detail-1" data-toggle="detail-1">
+                    <div class="col-xs-10">
+                   <a href="#"><i class="fas fa-file-alt"></i> Jadwal Ujian Tengah Semester (UTS) Genap T.A 2018-2019</a>
+                    </div>
+                    <div class="col-xs-2"><i class="fa fa-chevron-down pull-right" data-toggle="tooltip" data-placement="left" title="Lihat Deskripsi"></i></div>
+                </div>
+                <div id="detail-1">
+                   Berikut lampiran Jadwal Ujian Tengah Semester (UTS) Genap T.A 2018-2019   
+                </div>
+            </li> 
+            <li class="list-group-item">
+                <div class="row toggle" id="dropdown-detail-2" data-toggle="detail-2">
+                    <div class="col-xs-10">
+                   <a href="#"><i class="fas fa-file-alt"></i> Jadwal Seminar Tugas Akhir Periode Bulan Januari T.A 2018-2019</a>
+                    </div>
+                    <div class="col-xs-2"><i class="fa fa-chevron-down pull-right" data-toggle="tooltip" data-placement="left" title="Lihat Deskripsi"></i></div>
+                </div>
+                <div id="detail-2">
+                Berikut kami lampirkan Jadwal Seminar Tugas Akhir periode bulan Januari 2018-2019 
+                </div>
+            </li>
+            <li class="list-group-item">
+                <div class="row toggle" id="dropdown-detail-3" data-toggle="detail-3">
+                    <div class="col-xs-10">
+                   <a href="#"><i class="fas fa-file-alt"></i>  Pedoman Penulisan Laporan Tugas Akhir Teknik Informatika</a>
+                    </div>
+                    <div class="col-xs-2"><i class="fa fa-chevron-down pull-right" style="color:#a4a5a4"></i></div>
+                </div>
+                <div id="detail-3">
+                    
+                </div>
+            </li> 
+            <li class="list-group-item">
+                <div class="row toggle" id="dropdown-detail-4" data-toggle="detail-4">
+                    <div class="col-xs-10">
+                   <a href="#"><i class="fas fa-file-alt"></i> Pedoman Penulisan Laporan Tugas Akhir Teknik Sipil</a>
+                    </div>
+                    <div class="col-xs-2"><i class="fa fa-chevron-down pull-right" style="color:#a4a5a4"></i></div>
+                </div>
+                <div id="detail-4">
+                
+                </div>
+            </li>  
+            <li class="list-group-item">
+                <div class="row toggle" id="dropdown-detail-5" data-toggle="detail-5">
+                    <div class="col-xs-10">
+                   <a href="#"><i class="fas fa-file-alt"></i> Pedoman Penulisan Laporan Kerja Praktek Teknik Sipil</a>
+                    </div>
+                    <div class="col-xs-2"><i class="fa fa-chevron-down pull-right" style="color:#a4a5a4"></i></div>
+                </div>
+                <div id="detail-5">
+               
+                </div>
+            </li> 
+            <li class="list-group-item">
+                <div class="row toggle" id="dropdown-detail-6" data-toggle="detail-6">
+                    <div class="col-xs-10">
+                   <a href="#"><i class="fas fa-file-alt"></i> Jadwal Perkuliahan Genap T.A 2018-2019</a>
+                    </div>
+                    <div class="col-xs-2"><i class="fa fa-chevron-down pull-right" data-toggle="tooltip" data-placement="left" title="Lihat Deskripsi"></i></div>
+                </div>
+                <div id="detail-6">
+                Berikut Jadwal Perkuliahan Genap T.A 2018-2019. Perkuliahan akan dimulai pada tanggal 28 Januari 2019.
+                </div>
+            </li> 
+            <li class="list-group-item">
+                <div class="row toggle" id="dropdown-detail-7" data-toggle="detail-7">
+                    <div class="col-xs-10">
+                   <a href="#"><i class="fas fa-file-alt"></i> Jadwal Ujian Akhir Semester Ganjil T.A 2018-2019</a>
+                    </div>
+                    <div class="col-xs-2"><i class="fa fa-chevron-down pull-right" data-toggle="tooltip" data-placement="left" title="Lihat Deskripsi"></i></div>
+                </div>
+                <div id="detail-7">
+                Berikut Jadwal UAS Semester Ganjil T.A 2018-2019
+                </div>
+            </li> 
+            <li class="list-group-item">
+                <div class="row toggle" id="dropdown-detail-8" data-toggle="detail-8">
+                    <div class="col-xs-10">
+                   <a href="#"><i class="fas fa-file-alt"></i>  Revisi Jadwal UTS 2018-1</a>
+                    </div>
+                    <div class="col-xs-2"><i class="fa fa-chevron-down pull-right" data-toggle="tooltip" data-placement="left" title="Lihat Deskripsi"></i></div>
+                </div>
+                <div id="detail-8">
+                Berikut kami sampaikan Revisi Jadwal UTS 2018-1 untuk mata kuliah prodi Teknik Informatika <br>  1. Pemrograman Desktop IF-D dan IF-E 2016  <br> 2. Data Mining
+                </div>
+            </li> 
+            <li class="list-group-item">
+                <div class="row toggle" id="dropdown-detail-9" data-toggle="detail-9">
+                    <div class="col-xs-10">
+                   <a href="#"><i class="fas fa-file-alt"></i>  Berikut Jadwal Ujian Tengah Semester Ganjil T.A 2018-2019</a>
+                    </div>
+                    <div class="col-xs-2"><i class="fa fa-chevron-down pull-right" data-toggle="tooltip" data-placement="left" title="Lihat Deskripsi"></i></div>
+                </div>
+                <div id="detail-9">
+                Berikut Jadwal Ujian Tengah Semester Ganjil T.A 2018-2019. Mohon diperhatikan catatan berikut: <br>
+                1. UTS dilaksanakan mulai tanggal 29 Oktober 2018 s.d 10 November 2018 <br>
+                2. Pembayaran harap dilakukan sebelum tanggal UTS dimulai <br>
+                3. Tidak ada Ujian Susulan <br>
+                4. Pelaksanaan Ujian Susulan HANYA dilaksanakan oleh Dosen yang Bersangkutan dengan menyertakan Surat Sakit atau Surat Ket. Kerja (Kartu Ujian Dibawa) <br>
+                 5. Pelaksanaan Ujian Mahasiswa Bentrok dilaksanakan pada Hari yang sama, dan ruangan ditentukan oleh panitia. (Dengan memperlihatkan Bukti Jadwal Matkul yang Bentrok). 
+                </div>
+            </li> 
+            <li class="list-group-item">
+                <div class="row toggle" id="dropdown-detail-10" data-toggle="detail-10">
+                    <div class="col-xs-10">
+                   <a href="#"><i class="fas fa-file-alt"></i> Revisi 03 Matrik Perkuliahan Semester Genap TA 2018 2019</a>
+                    </div>
+                    <div class="col-xs-2"><i class="fa fa-chevron-down pull-right" style="color:#a4a5a4"></i></div>
+                </div>
+                <div id="detail-10">
+                   
+                </div>
+            </li> 
+            <li class="list-group-item">
+                <div class="row toggle" id="dropdown-detail-11" data-toggle="detail-11">
+                    <div class="col-xs-10">
+                   <a href="#"><i class="fas fa-file-alt"></i> Jadwal Kuliah TA 2018 2019 Revisi 02</a>
+                    </div>
+                    <div class="col-xs-2"><i class="fa fa-chevron-down pull-right" data-toggle="tooltip" data-placement="left" title="Lihat Deskripsi"></i></div>
+                </div>
+                <div id="detail-11">
+                Berikut kami sampaikan perubahan Jadwal Revisi 02 
+                </div>
+            </li> 
+            <li class="list-group-item">
+                <div class="row toggle" id="dropdown-detail-12" data-toggle="detail-12">
+                    <div class="col-xs-10">
+                   <a href="#"><i class="fas fa-file-alt"></i> Jadwal Kuliah Teknik Industri TA. 2018-2019</a>
+                    </div>
+                    <div class="col-xs-2"><i class="fa fa-chevron-down pull-right" style="color:#a4a5a4"></i></div>
+                </div>
+                <div id="detail-12">
+                   
+                </div>
+            </li> 
+            <li class="list-group-item">
+                <div class="row toggle" id="dropdown-detail-13" data-toggle="detail-13">
+                    <div class="col-xs-10">
+                   <a href="#"><i class="fas fa-file-alt"></i> Jadwal Kuliah Teknik Sipil TA. 2018-2019</a>
+                    </div>
+                    <div class="col-xs-2"><i class="fa fa-chevron-down pull-right" style="color:#a4a5a4"></i></div>
+                </div>
+                <div id="detail-13">
+                  
+                </div>
+            </li> 
+            <li class="list-group-item">
+                <div class="row toggle" id="dropdown-detail-14" data-toggle="detail-14">
+                    <div class="col-xs-10">
+                   <a href="#"><i class="fas fa-file-alt"></i> Jadwal Perkuliahan Ganjil Teknik Informatika</a>
+                    </div>
+                    <div class="col-xs-2"><i class="fa fa-chevron-down pull-right" data-toggle="tooltip" data-placement="left" title="Lihat Deskripsi"></i></div>
+                </div>
+                <div id="detail-14">
+                Berikut Jadwal Perkuliahan Semester Ganjil Tahun Akademik 2018-2019  
+                </div>
+            </li> 
+            <li class="list-group-item">
+                <div class="row toggle" id="dropdown-detail-15" data-toggle="detail-15" style="color:#a4a5a4">
+                    <div class="col-xs-10">
+                   <a href="#"><i class="fas fa-file-alt"></i> Kalender Akademik TA. 2018-2019</a>
+                    </div>
+                    <div class="col-xs-2"><i class="fa fa-chevron-down pull-right" style="color:#a4a5a4"></i></div>
+                </div>
+                <div id="detail-15">
+                   
+                </div>
+            </li> 
+            <li class="list-group-item">
+                <div class="row toggle" id="dropdown-detail-16" data-toggle="detail-16">
+                    <div class="col-xs-10">
+                   <a href="#"><i class="fas fa-file-alt"></i> Jadwal Konseling Perwalian Semester Ganjil TA. 2018-2019</a>
+                    </div>
+                    <div class="col-xs-2"><i class="fa fa-chevron-down pull-right" data-toggle="tooltip" data-placement="left" title="Lihat Deskripsi"></i></div>
+                </div>
+                <div id="detail-16">
+                1. Pelaksanaan Perwalian tanggal 03 s.d. 08 September 2018 <br> 
+                2. Add & Drop Mata Kuliah: Tanggal 24 s.d. 29 September 2018 Syarat Perwalian: <br> 
+                <span class="tab">1) Telah melakukan pembayaran ke rekening Fakultas Teknik (Reguler Rp. 150.000, Non Reguler Rp. 300.000) </span> <br>
+                <span class="tab">2) Menyerahkan bukti pembayaran e BAAK </span> <br>
+                <span class="tab">3) Melakukan Perwalian Online (http://ft.unsur.ac.id) </span> <br>
+                <span class="tab">4) Membawa Form Hasil Studi (FHS) yang sudah di setujui Dosen Wali di Perwalian Online </span>
+                </div>
+            </li> 
+            <li class="list-group-item">
+                <div class="row toggle" id="dropdown-detail-17" data-toggle="detail-17">
+                    <div class="col-xs-10">
+                   <a href="#"><i class="fas fa-file-alt"></i> Formulir Pendaftaran SIDANG TA INFORMATIKA</a>
+                    </div>
+                    <div class="col-xs-2"><i class="fa fa-chevron-down pull-right" data-toggle="tooltip" data-placement="left" title="Lihat Deskripsi"></i></div>
+                </div>
+                <div id="detail-17">
+                Bagi Mahasiswa yang akan melaksanakan Sidang Tugas Akhir silahkan unduh Formulir untuk pendaftaran Sidang Tugas Akhir Program Studi Teknik Informatika  
+                </div>
+            </li> 
+            <li class="list-group-item">
+                <div class="row toggle" id="dropdown-detail-18" data-toggle="detail-18">
+                    <div class="col-xs-10">
+                   <a href="#"><i class="fas fa-file-alt"></i> Formulir Pendaftaran SEMINAR TA INFORMATIKA</a>
+                    </div>
+                    <div class="col-xs-2"><i class="fa fa-chevron-down pull-right" data-toggle="tooltip" data-placement="left" title="Lihat Deskripsi"></i></div>
+                </div>
+                <div id="detail-18">
+                Bagi Mahasiswa yang akan melaksanakan Seminar Tugas Akhir silahkan unduh Formulir untuk Pendaftaran pelaksanaan Seminar Tugas Akhir Teknik Informatika  
+                </div>
+            </li> 
+            <li class="list-group-item">
+                <div class="row toggle" id="dropdown-detail-19" data-toggle="detail-19">
+                    <div class="col-xs-10">
+                   <a href="#"><i class="fas fa-file-alt"></i> Formulir Pendaftaran KP INFORMATIKA</a>
+                    </div>
+                    <div class="col-xs-2"><i class="fa fa-chevron-down pull-right" data-toggle="tooltip" data-placement="left" title="Lihat Deskripsi"></i></div>
+                </div>
+                <div id="detail-19">
+                Bagi Mahasiswa yang akan melaksanakan Seminar Kerja Praktek silahkan unduh Formulir Pendaftaran untuk Seminar Kerja Praktek Teknik Informatika
+                </div>
+            </li> 
+            <li class="list-group-item">
+                <div class="row toggle" id="dropdown-detail-20" data-toggle="detail-20">
+                    <div class="col-xs-10">
+                   <a href="#"><i class="fas fa-file-alt"></i> Pengajuan Judul Seminar KP TA Program Studi Teknik Industri</a>
+                    </div>
+                    <div class="col-xs-2"><i class="fa fa-chevron-down pull-right" data-toggle="tooltip" data-placement="left" title="Lihat Deskripsi"></i></div>
+                </div>
+                <div id="detail-20">
+                Bagi mahasiswa yang akan mengajukan judul KP & TA, diharapkan kepada mahasiswa yang sudah mengambil/ kontrak Mata Kuliah Seminar KP, Seminar TA, & Sidang TA, untuk segera mengisi form ini, untuk mempercepat pembagian pembimbing.
+                </div>
+            </li> 
+            <li class="list-group-item li1">
+                <div class="row toggle" id="dropdown-detail-21" data-toggle="detail-21">
+                    <div class="col-xs-10">
+                   <a href="#"><i class="fas fa-file-alt"></i> Pedoman Penulisan KP & TA Program Studi Teknik Industri</a>
+                    </div>
+                    <div class="col-xs-2"><i class="fa fa-chevron-down pull-right" data-toggle="tooltip" data-placement="left" title="Lihat Deskripsi"></i></div>
+                </div>
+                <div id="detail-21">
+                Pedoman ini digunakan apabila Mahasiswa sedang mengambil KP & TA, dihimbau kepada Mahasiswa untuk format penulisan KP & TA mengacu ke pedoman ini. Apabila Mahasiswa disaat sedang melakukan penyusunan laporan KP & TA diluar dari format penulisan ini, maka tidak akan di <strong>ACC</strong>  oleh pembimbing <strong>KP & TA !!!</strong> 
+                </div>
+            </li> 
+                  
+        </div>
+    </div>  
+    
+    <div id=menuProfil>
+    <div class="row">
+         <div class="col-sm-12">
+            <hr class="hr-primary" />
+            <ol class="breadcrumb bread-primary ">   
+              
+             </ol>
+         </div>
+     </div>
+     </div>
+    
+     <script src="js/jquery.magnify.js"></script>
+    
+    <script>
+        $('[data-magnify]').magnify({
+        resizable: false,
+         headToolbar: [
+            'close'
+        ],
+        initMaximized: true
+        })
+    </script>
+
+    <script>
+            $(document).ready(function() {
+                $('[id^=detail-]').hide();
+                 $('.toggle').click(function() {
+                  $input = $( this );
+                  $target = $('#'+$input.attr('data-toggle'));
+                 $target.slideToggle();
+                     });
+                    });
+    </script>
+
+    <script >
+           var btn = $('#button');
+
+      $(window).scroll(function() {
+      if ($(window).scrollTop() > 300) {
+      btn.addClass('show');
+      } else {
+      btn.removeClass('show');
+      }
+    });
+
+    btn.on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({scrollTop:0}, '300');
+    });
+    </script>
+
+    <script>
+        $(function () {
+  $('[data-toggle="tooltip"]').tooltip();
+});
+    </script>
+</div>
